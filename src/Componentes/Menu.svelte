275fab742500969b4b48b67e24e25cb4d46f6a6b
pedro-i-M-a <p007bon@gmail.com>
2024-02-07 16:05:05 -0600

    update

---
+++ src/Componentes/Menu.svelte
@@ -0,0 +1,84 @@
+<script>
+  import {link} from 'svelte-spa-router'
+ 
+
+</script>
+
+<main>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <img src="img/logo-la-clinica-dental-oscuro.png" alt="">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="!#">Tratamientos</a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="nav-link" href="!#">NOSOTROS</a>
+              </li> -->
+              <li class="nav-item">
+                <a class="nav-link" use:link href="/Tratamientos">Sucursales</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="!#">Contáctanos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="!#">Nosotros</a>
+              </li>
+              <li class="nav-item">
+                <!-- <a class="nav-link" use:link href="/Contactanos">CONTÁCTANOS</a> -->
+              </li>
+              
+            </ul>
+            <button class="btn agenda">
+                <i class="bi bi-whatsapp"></i>
+                Cotiza tu Tratamiento
+            </button>
+            <button class="btn llamada">
+                <i class="bi bi-telephone"></i>
+                Llámanos
+            </button>
+          </div>
+        </div>
+      </nav>
+</main>
+
+<style>
+    /* COLOR QUE SE USA en facial #F3D3A4*/
+    .agenda{
+        background-color:#F3D3A4;
+        color: white;
+        margin-right: 10px;
+      }
+      .llamada{
+      margin-right: 200px;
+        background-color:#F3D3A4; 
+        color: white;
+    }
+
+   
+
+    /* HOVER DEL nav-item a */
+    .navbar-nav .nav-item.active a,
+  .navbar-nav .nav-item:hover a {
+    color: #F3D3A4; /* Color de texto al seleccionar o pasar el ratón por encima */
+    border-bottom: 2px solid #F3D3A4; /* Borde inferior al seleccionar o pasar el ratón por encima */
+  }
+
+  .navbar-expand-lg{
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    background-color: rgba(149, 230, 255, 0.3);
+    /* color: white; */
+    text-align: center;
+    z-index: 1;
+  }
+  .collapse{
+    margin-left: 500px;
+  }
+</style>
---
+++ src/Principal/Inicio.svelte
@@ -0,0 +1,109 @@
+<script>
+
+</script>
+
+<main>
+<!-- derecha -->
+<div class="card mb-3" >
+    <div class="row g-0">
+        <div class="col-md-4 order-md-1"> <!-- Añade la clase order-md-1 para controlar el orden en pantallas medianas y grandes -->
+            <img src="img/toxi.png" class="img-fluid rounded-start img" alt="...">
+        </div>
+        <div class="col-md-8 order-md-0"> <!-- Añade la clase order-md-0 para controlar el orden en pantallas medianas y grandes -->
+            <div class="card-body">
+                <h5 class="card-title"><strong>Toxina Botulínica</strong></h5>
+                <p class="card-text">Nuestro tratamiento con botox es una opción segura y efectiva para suavizar las arrugas y líneas de expresion, brindándote un aspecto más juvenil y fresco.</p>
+            </div>
+            <div class="btn-trata">
+                <button class="btn btn-tra ">
+                  <i class="bi bi-whatsapp"></i>
+                  Cotiza tu Tratamiento
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- izquierda -->
+<div class="card mb-3" >
+    <div class="row g-0">
+        <div class="col-md-4 "> <!-- Añade la clase order-md-1 para controlar el orden en pantallas medianas y grandes -->
+            <img src="img/toxi.png" class="img-fluid rounded-start img" alt="...">
+        </div>
+        <div class="col-md-8 order-md-0"> <!-- Añade la clase order-md-0 para controlar el orden en pantallas medianas y grandes -->
+            <div class="card-body">
+                <h5 class="card-title"><strong>¿Qué es la Toxina Botulínica?</strong></h5>
+                <p class="card-text">La Toxina Botulínica es una proteína que relaja temporalmente los músculos responsables de las arrugas dinámicas.</p>
+                <p>Al bloquear las señales nerviosas que provocan la contracción muscular, logramos suavisar las líneas y arrugas, brindándote un aspecto más
+                    juvenil y descansado.
+                </p>
+                <p>La toxina botulínica debe ser administrada por profecionales médicos calificados para garantizar la seguridad 
+                    y eficacia del traatamiento. Admás, los resultados son temporales y generalmente duran varios meses, por lo que 
+                    pueden requerirse tratamientos de seguimiento para mantener los beneficios.
+                </p>
+            </div>
+            <div class="btn-trata">
+                <button class="btn btn-tra ">
+                  <i class="bi bi-whatsapp"></i>
+                  Cotiza tu Tratamiento
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- DIV DE BENEFICIOS -->
+<div>
+    <img src="img/beneficios.png" alt="">
+</div>
+
+<!-- ANUNCIO DE COTIZACION -->
+<div class="cotiza">
+    <h2>¡REJUVENECE TU ROSTRO HOY!</h2>
+    <div class="btn-coti">
+        <button class="btn btn-cot ">
+            <i class="bi bi-whatsapp"></i>
+            Cotiza tu Tratamiento
+        </button>
+    </div>
+</div>
+</main>
+
+<style>
+.btn-tra{
+    background-color:#F3D3A4;
+    color: white;
+    margin-left: 20px;
+    margin-top: 20px;
+}
+/* ESTILOS DE LA SECCION DE COTIZAR */
+.cotiza{
+    display: grid;
+    grid-template-columns: auto auto;
+    background-color:#F3D3A4;
+    padding-top: 40px;
+    padding-bottom: 40px;
+}
+.cotiza h2{
+    margin-left: 200px;
+    color: white;
+}
+.btn-coti{
+    display: flex;
+    align-items: center ;
+}
+.btn-cot{
+background-color: #424242;
+color: white;
+padding-left: 100px;
+padding-right: 100px;
+}
+
+
+.img{
+    width: 50%;
+    height: auto;
+}
+img{
+    width: 100%;
+}
+</style>
