2c370961e07e1a39fc3912732ede799ea64f5ad3
pedro <p007bon@gmail.com>
2024-02-14 10:57:46 -0600

    update de banner

---
+++ src/Carruseles/BannerPrincipal.svelte
@@ -0,0 +1,57 @@
+<script>
+
+</script>
+
+<main>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img-banners/tratamientos-faciales-1.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>ÁCIDO HIALURÓNICO</h5>
+                <p>Mejora la calidad de la piel. Disminuye las arrugas finas, consigue un efecto aumentado colágeno y elastina.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img-banners/tratamientos-faciales-2.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>NANO NEEDLE</h5>
+                <p>Aumenta la absorción de nutrientes de la piel para lucirla con mayor hidratación.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img-banners/tratamientos-faciales-3.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>TOXINA BOTULÍNICA</h5>
+                <p>Nuestra Clínica Facial de cuidado de la piel trata a cada cliente, hombre o mujer, con la mejor atención.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img-banners/tratamientos-faciales-4.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>DISEÑO DE SONRISAS</h5>
+                <p>Transforma tu sonrisa con nuestros tratamientos de diseño de sonrisas y complementa tu tratamiento facial.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+</main>
+
+<style>
+.carousel-caption{
+    margin-bottom: 10%;
+    font-size: 1.5rem;
+    text-shadow: 1px 1px 1px black;
+}
+.carousel-caption h5{
+font-size: 3rem;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 // @ts-nocheck
 import Inicio from './Principal/Inicio.svelte'
+import Toxina from './Principal/Toxina.svelte'
 
 const routes = {
     '/': Inicio,
+    '/Toxina Botulínica':Toxina,
 }
 
 export default routes
